dfc052630a026a23096f7d39c28b0fcdbeceac6a
chuck 2.0 <bozicslxye1@gmail.com>
2018-02-28 18:47:21 +0300

    added the test files

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # freshred5
 A simple site for the red five company
  Developing the backend code in python/Flask for easy deployment.
-
--- app.py
+++ app.py
@@ -1,20 +1,36 @@
 # just write the scrapper with in this script bro.
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 app = Flask(__name__)
-
-
 
 
 @app.route('/', methods=['GET'])
 def index():
     # for item in agric_prices:
-    return  render_template('index.html')
-
+    return render_template('index.html')
 
 
 @app.route('/dinnershow')
 def dinnershow():
     return render_template('dinnershow.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login1.html')
+
+@app.route('/datahander', methods=["GET", "POST"])
+def data():
+    if request.method == "POST":
+        values = request.form
+        for i in values:
+            return i
+
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
 
 
 if __name__ == '__main__':

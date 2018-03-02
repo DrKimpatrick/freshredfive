a6fbfc4f6eb38a40184e71b4ee2edd8a3e2232d4
chuck 2.0 <bozicslxye1@gmail.com>
2018-03-02 12:01:17 +0300

    added an authentication branch

---
+++ data.py
@@ -0,0 +1,27 @@
+def Articles():
+    articles = [
+    {
+        'id':1,
+        'title':'Article 1',
+        'body':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, blanditiis sed molestias illo rerum hic ad ullam, excepturi quasi cupiditate vel! Incidunt illo consectetur fugit repudiandae, et a perferendis magnam.',
+        'author':'chucky',
+        'create_date':'20-11-2017'
+    },
+    {
+        'id':2,
+        'title':'Article 2',
+        'body':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, blanditiis sed molestias illo rerum hic ad ullam, excepturi quasi cupiditate vel! Incidunt illo consectetur fugit repudiandae, et a perferendis magnam.',
+        'author':'Bozics',
+        'create_date':'21-11-2017'
+    },
+    {
+        'id':3,
+        'title':'Article 3',
+        'body':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, blanditiis sed molestias illo rerum hic ad ullam, excepturi quasi cupiditate vel! Incidunt illo consectetur fugit repudiandae, et a perferendis magnam.',
+        'author':'Mobby',
+        'create_date':'24-11-2017'
+    }
+
+    ]
+
+    return articles

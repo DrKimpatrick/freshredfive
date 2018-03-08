ec96a3b1667239bf997531af1f34fdf7ca895e32
chuck 2.0 <bozicslxye1@gmail.com>
2018-03-08 11:26:49 +0300

    removed boilerplate code

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template, flash, redirect, url_for, session,request
+from flask import Flask, render_template, flash, redirect, url_for, session, request
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from wtforms.validators import DataRequired
@@ -10,7 +10,7 @@ app = Flask(__name__)
 #Articles = Articles()
 
 # config mysql
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 #db = SQLAlchemy(app)
 
 app.config['MYSQL_HOST'] = 'localhost'
@@ -45,6 +45,8 @@ def index():
     return render_template('index.html')
 
 # index recipes
+
+
 @app.route('/recipes')
 def recipes():
     return render_template('recipes.html')
@@ -56,32 +58,41 @@ def lunchbox():
     return render_template('lunchbox.html')
 
 # index dinnershow
+
+
 @app.route('/dinnershow')
 def dinnershow():
     return render_template('dinnershow.html')
 
 # index cookschool
+
+
 @app.route('/cookschool')
 def cookschool():
     return render_template('cookschool.html')
 
 # index openhouse
+
+
 @app.route('/openhouse')
 def openhouse():
     return render_template('openhouse.html')
 
 
-#usageclub
+# usageclub
 @app.route('/usageclub')
 def usageclub():
     return render_template('usageclub.html')
 
-#life changing route
+# life changing route
+
+
 @app.route('/lifechanging')
 def lifechanging():
     return render_template('lifechanging.html')
 
 # blog route
+
 
 @app.route('/blog')
 def blog():
@@ -96,7 +107,7 @@ def blog():
     newest = reversed(articles)
 
     if result > 0:
-        return render_template('blog.html', articles=articles,random_article=random_article,newest=newest)
+        return render_template('blog.html', articles=articles, random_article=random_article, newest=newest)
 
     else:
         msg = "No articles where found"
@@ -107,14 +118,14 @@ def blog():
     return render_template('blog.html', articles=articles)
 
 
-
-
 # about route
 @app.route('/about')
 def about():
     return render_template('about.html')
 
 # login route
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -138,7 +149,6 @@ def login():
     return render_template('login.html')
 
 
-
 # dashboard
 @app.route('/dashboard')
 @is_logged_in
@@ -160,7 +170,7 @@ def dashboard():
     cur.close()
 
 
-#creating articles form
+# creating articles form
 class ArticleForm(Form):
     title = StringField('Title', validators=[
                         DataRequired(), validators.Length(min=4, max=250)])
@@ -169,27 +179,24 @@ class ArticleForm(Form):
                          DataRequired(), validators.Length(min=30)])
 
 
-
-
-#edit article
+# edit article
 @app.route('/edit_article/<string:id>', methods=["GET", "POST"])
 @is_logged_in
 def edit_article(id):
-    #create cursor
+    # create cursor
     cur = mysql.connection.cursor()
 
-    #get article by id
+    # get article by id
     result = cur.execute("SELECT * FROM articles WHERE id = %s", [id])
     article = cur.fetchone()
 
-    #get the form
+    # get the form
     form = ArticleForm(request.form)
 
     form.title.data = article['title']
     form.body.data = article['body']
 
-    #fill the form of the fields
-
+    # fill the form of the fields
 
     if request.method == "POST" and form.validate():
         title = request.form['title']
@@ -199,8 +206,8 @@ def edit_article(id):
         cur = mysql.connection.cursor()
 
         # execute
-        cur.execute("UPDATE  articles SET title=%s,body=%s WHERE id=%s",(title,body,id))
-
+        cur.execute(
+            "UPDATE  articles SET title=%s,body=%s WHERE id=%s", (title, body, id))
 
         # commit to db
         mysql.connection.commit()
@@ -213,15 +220,17 @@ def edit_article(id):
 
     return render_template('edit_article.html', form=form)
 
-#delete articles
-@app.route('/delete_article/<string:id>', methods=['GET','POST'])
+# delete articles
+
+
+@app.route('/delete_article/<string:id>', methods=['GET', 'POST'])
 @is_logged_in
 def delete_article(id):
-    #create db cursor
+    # create db cursor
     cur = mysql.connection.cursor()
 
-    #Execute
-    cur.execute("DELETE FROM articles WHERE id=%s",[id])
+    # Execute
+    cur.execute("DELETE FROM articles WHERE id=%s", [id])
 
     # commit to db
     mysql.connection.commit()
@@ -231,7 +240,6 @@ def delete_article(id):
 
     flash('Article deleted', 'success')
     return redirect(url_for('dashboard'))
-
 
 
 @app.route('/add_article', methods=["GET", "POST"])
@@ -262,6 +270,8 @@ def add_article():
     return render_template('add_article.html', form=form)
 
 # articles route
+
+
 @app.route('/articles')
 def articles():
 
@@ -280,9 +290,11 @@ def articles():
 
     cur.close()
 
-    #return render_template('articles.html',articles=Articles)
+    # return render_template('articles.html',articles=Articles)
 
 # article route
+
+
 @app.route('/article/<string:id>/')
 def article(id):
     cur = mysql.connection.cursor()
@@ -299,12 +311,15 @@ def article(id):
         return redirect(url_for('index'))
 
 # about log out
+
+
 @app.route('/logout')
 @is_logged_in
 def logout():
     session.clear()
     flash('You are now logged out', 'success')
     return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)

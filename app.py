089378f5e0e56a35bea43bd1ee5d70606d73f953
chuck 2.0 <bozicslxye1@gmail.com>
2018-03-15 20:32:27 +0300

    add os environment variables

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ def dinnershow():
            {
              "to": [
                {
-                 "email": "bozicslxye1@gmail.com"
+                 "email": "alexshyaka@therailsshop.club"
                }
              ],
              "subject": "Fresh red 5 ordering recipes"
